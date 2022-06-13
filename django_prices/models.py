04d8f4abdc813113dd5dd666fe2e3591916d795b
John Barry <john.barry@gmail.com>
2022-06-13 15:25:29 -0500

    remove context

--- django_prices/models.py
+++ django_prices/models.py
@@ -11,7 +11,69 @@ from functools import total_ordering
 @total_ordering
 class NonDatabaseFieldBase:
     """Base class for all fields that are not stored in the database."""
+    description = 'A field that stores an amount of money'
 
+    def __init__(self, verbose_name=None, currency=None, **kwargs):
+        self.currency = currency
+        super(MoneyField, self).__init__(verbose_name, **kwargs)
+
+    def from_db_value(self, value, expression, connection):
+        return self.to_python(value)
+
+    def to_python(self, value):
+        if isinstance(value, Money):
+            if value.currency != self.currency:
+                raise ValueError('Invalid currency: %r (expected %r)' % (
+                    value.currency, self.currency))
+            return value
+        value = super(MoneyField, self).to_python(value)
+        if value is None:
+            return value
+        return Money(value, self.currency)
+
+    def get_prep_value(self, value):
+        value = self.to_python(value)
+        if value is not None:
+            return value.amount
+        return value
+
+    def get_db_prep_save(self, value, connection):
+        value = self.get_prep_value(value)
+        db_value = connection.ops.adapt_decimalfield_value
+        return db_value(value, self.max_digits, self.decimal_places)
+
+    def value_to_string(self, obj):
+        value = self.value_from_object(obj)
+        if value is not None:
+            return value
+        return super(MoneyField, self).value_to_string(value)
+
+    def formfield(self, **kwargs):
+        defaults = {'currency': self.currency, 'form_class': forms.MoneyField}
+        defaults.update(kwargs)
+        return super(MoneyField, self).formfield(**defaults)
+
+    def get_default(self):
+        default = super(MoneyField, self).get_default()
+        return self.to_python(default)
+
+    @cached_property
+    def validators(self):
+        validators = list(
+            itertools.chain(self.default_validators, self._validators))
+        return validators + [MoneyPrecisionValidator(
+            self.currency, self.max_digits, self.decimal_places)]
+
+    def deconstruct(self):
+        name, path, args, kwargs = super(MoneyField, self).deconstruct()
+        kwargs['currency'] = self.currency
+        return name, path, args, kwargs
+
+
+class TaxedMoneyField(object):
+
+    description = (
+        'A field that combines net and gross fields values into TaxedMoney.')
     empty_values = list(validators.EMPTY_VALUES)
 
     # Field flags
